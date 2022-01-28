fe579b2b8e8c64decaa2e7ca02cdb764d2b41c95
Bubild <chekanov.n@inbox.ru>
2022-01-29 00:38:25 +0300

    First console program

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("hello world!")
-print("GIT test")
+class Main:
+    def __init__(self):
+        self.printHello()
+
+    def printHello(self):
+        input("Привет, ")
+
+if __name__ == "__main__":
+    program = Main()
