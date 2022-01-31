390189614c9e2ff57b09dfc5a44399986a769d2b
Bubild <chekanov.n@inbox.ru>
2022-01-31 17:47:00 +0300

    Сделал черное окно

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-class Main:
-    def __init__(self):
-        self.printHello()
+import pygame
+import ctypes
 
-    def printHello(self):
-        input("Привет, ")
+class Main:
+
+    pygame.display.set_mode((
+        ctypes.windll.user32.GetSystemMetrics(0),
+        ctypes.windll.user32.GetSystemMetrics(1)
+    ))
+    pygame.display.set_caption("Shaurma Simulator")
+
+    pygame.time.Clock().tick(60)
+
+    run = True
+
+    def __init__(self):
+
+        pygame.init()
+
+        while self.run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+                    pygame.quit()
+
+            self.keys = pygame.key.get_pressed()
+
+            if self.keys[pygame.K_ESCAPE]:
+                self.run = False
+                pygame.quit()
 
 if __name__ == "__main__":
     program = Main()
